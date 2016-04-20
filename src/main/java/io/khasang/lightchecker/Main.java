7f7bff9ed2dd0a5172550ba00acfbca9c52c50c3
truesik <truesik@gmail.com>
2016-04-20 20:41:47 +0300

    fix

--- src/main/java/io/khasang/lightchecker/Main.java
+++ src/main/java/io/khasang/lightchecker/Main.java
@@ -3,7 +3,9 @@ package io.khasang.lightchecker;
 import io.khasang.lightchecker.light.GreenLight;
 import io.khasang.lightchecker.light.RedLight;
 import io.khasang.lightchecker.light.YellowLight;
-import io.khasang.lightchecker.model.TrafficLight;
+import io.khasang.lightchecker.trafficlight.LightChecker;
+import io.khasang.lightchecker.trafficlight.MinutesReader;
+import io.khasang.lightchecker.trafficlight.TrafficLight;
 
 public class Main {
     public static void main(String[] args) {
