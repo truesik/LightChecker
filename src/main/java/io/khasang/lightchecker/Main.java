de939a802c314892d7703eb6331967b43357cd7e
truesik <truesik@gmail.com>
2016-04-20 20:19:30 +0300

    точка входа

---
+++ src/main/java/io/khasang/lightchecker/Main.java
@@ -0,0 +1,13 @@
+package io.khasang.lightchecker;
+
+import io.khasang.lightchecker.light.GreenLight;
+import io.khasang.lightchecker.light.RedLight;
+import io.khasang.lightchecker.light.YellowLight;
+import io.khasang.lightchecker.model.TrafficLight;
+
+public class Main {
+    public static void main(String[] args) {
+        LightChecker lightChecker = new LightChecker(new TrafficLight(new GreenLight(2), new YellowLight(3), new RedLight(5)));
+        System.out.println(lightChecker.getLightColor(new MinutesReader().read()));
+    }
+}
